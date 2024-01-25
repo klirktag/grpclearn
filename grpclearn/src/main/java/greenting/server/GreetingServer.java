982148ebabfca16bc8ba1b53c5ceded6d7fe489f
klirktag <klirktag@protonmail.com>
2024-01-25 15:11:59 +0100

    add server

---
+++ grpclearn/src/main/java/greenting/server/GreetingServer.java
@@ -0,0 +1,25 @@
+package greenting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int port = 50051;
+
+        Server server = ServerBuilder.forPort(port).build();
+
+        server.start();
+
+        System.out.println("Server started");
+        System.out.println("listening on port "+port);
+        Runtime.getRuntime().addShutdownHook(new Thread(()-> {
+            System.out.println("Received shutdown request");
+            server.shutdown();
+            System.out.println("Server stopped");
+        }));
+        server.awaitTermination();
+    }
+}
